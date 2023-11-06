bc3f1ddd1e041cc3113f22a71c75a0f661e4681f
Larry Le <lle114@student.hbuhsd.edu>
2023-11-06 11:51:52 -0800

    answers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Cookie from './assets/cookie.png'
+import React, { useRef } from "react"
 
 // For this assignment you're going to make the cookie image do ONE 360 spin whenever it is clicked
 
@@ -11,9 +12,22 @@ import Cookie from './assets/cookie.png'
 
 function App() {
 
+  const cookie = useRef();
+
+  let spinCookie = () => {
+
+    if (cookie.current.classList.contains("spin")) return;
+    cookie.current.classList.add("spin");
+
+    // Remove class after it finishes
+    setTimeout(() => {
+      cookie.current.classList.remove("spin");
+    }, 1000);
+  }
+
   return (
     <div className="app">
-      <img alt="cookie" className="spin" src={Cookie} draggable="false" />
+      <img alt="cookie" src={Cookie} draggable="false" ref={cookie} onClick={spinCookie} />
     </div>
   );
 }
